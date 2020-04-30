299eea99a98571b07715b8b05a165cf465556f3b
Sushant Raut <raut.sushant@gmail.com>
2020-04-30 17:34:30 -0500

    prescription printing allignment

--- src/main/java/com/ekdant/dentalsolution/printing/Priscription.java
+++ src/main/java/com/ekdant/dentalsolution/printing/Priscription.java
@@ -8,6 +8,7 @@ package com.ekdant.dentalsolution.printing;
 
 import com.ekdant.dentalsolution.dao.CheckUpDao;
 import com.ekdant.dentalsolution.dao.ClinicDao;
+import com.ekdant.dentalsolution.dao.SettingsDao;
 import com.ekdant.dentalsolution.domain.CheckupBean;
 import com.ekdant.dentalsolution.domain.ClinicBean;
 import com.ekdant.dentalsolution.domain.PriscriptionBean;
@@ -42,6 +43,8 @@ public class Priscription extends Canvas implements Printable{
     int patientTreatmentId = 0;
     CheckUpDao checkUpDao;
     ClinicDao clinicDao;
+    SettingsDao settingsDao;
+    int pageCenter = 300;
     DateFormat displayDateFormat = new SimpleDateFormat(PropertiesCache.getInstance().getProperty("format.displaydate"));
     final static Logger logger = Logger.getLogger(Priscription.class);
     
@@ -49,7 +52,7 @@ public class Priscription extends Canvas implements Printable{
         patientTreatmentId = treatmentId;
         checkUpDao = new CheckUpDao();
         clinicDao = new ClinicDao();
-        
+        settingsDao = new SettingsDao();
     }
 
     public int print(Graphics gx, PageFormat pf, int page) throws PrinterException {
@@ -63,17 +66,38 @@ public class Priscription extends Canvas implements Printable{
         printHeader(g);
         printPatientInfo(g, checkup);
         printMedicine(g, checkup);
-       
+        printFooter(g);
         return PAGE_EXISTS; 
     }
     
-
-    private void printHeader(Graphics g) {
-        int pageCenter = 300;
+    private void printFooter(Graphics g){
+        
+        Font fontClinicName = new Font("Serif", Font.PLAIN, 12);
         List<ClinicBean> clinics = clinicDao.fetchClinics();
         ClinicBean clinic = clinics.get(0);
-        String clinicName = clinic.getName();
-        String clinicAddress = clinic.getAddress() + " , " +clinic.getCity();
+        String clinicAddr = clinic.getAddress() + " , " +clinic.getCity();
+
+        AttributedString clinicAddress = new AttributedString(clinicAddr);
+        clinicAddress.addAttribute(TextAttribute.FONT, fontClinicName);
+        clinicAddress.addAttribute(TextAttribute.FOREGROUND, Color.BLUE);
+                
+        g.setFont(fontClinicName);
+        g.drawLine(10, 700, 550, 700);
+        g.drawString(clinicAddress.getIterator(), pageCenter - getLength(clinicAddr, g) / 2, 730);
+        
+    }
+
+    private void printHeader(Graphics g) {
+        
+        List<ClinicBean> clinics = clinicDao.fetchClinics();
+        ClinicBean clinic = clinics.get(0);
+        String clinicName = settingsDao.getSettingValue("CLINIC_NAME");
+        String doctorNm = settingsDao.getSettingValue("DOCTOR_NAME");
+        String doctorContact = settingsDao.getSettingValue("DOCTOR_MOBILE");
+        String doctorEmail = settingsDao.getSettingValue("DOCTOR_EMAIL");
+        String doctorDegree = settingsDao.getSettingValue("DOCTOR_DEGREES");
+        String doctorReg = settingsDao.getSettingValue("DOCTOR_REGISTRATION_NO");
+        
         String contactNumber = clinic.getContact();
         URL iconURL = getClass().getResource("/EkDant/icones/priscription.jpg");
         Image img;
@@ -83,35 +107,51 @@ public class Priscription extends Canvas implements Printable{
             try {
                 img = ImageIO.read(iconURL);
                 
-                g.drawImage(img, 15, 0, 65, 50, 0, 0, 50, 50, null);
+                g.drawImage(img, 480, 10, 530, 60, 0, 0, 50, 50, null);
              } catch (IOException ex) {
                 logger.error("file not found");
              }
         }
-        Font fontClinicName = new Font("Serif", Font.BOLD, 20);
-        Font fontClinicAddress = new Font("Serif", Font.PLAIN, 10);        
-        Font fontClinicCont = new Font("Serif", Font.BOLD, 12);
+        Font fontClinicName = new Font("Serif", Font.BOLD, 25);
+        Font fontDoctorDetails = new Font("Serif", Font.BOLD, 10); 
+        Font fontDoctorNameDetails = new Font("Serif", Font.BOLD, 12);
         
 
         AttributedString clinicNm = new AttributedString(clinicName);
         clinicNm.addAttribute(TextAttribute.FONT, fontClinicName);
-        clinicNm.addAttribute(TextAttribute.FOREGROUND, Color.BLUE);
+        clinicNm.addAttribute(TextAttribute.FOREGROUND, Color.MAGENTA);
         
-        AttributedString clinicAdd = new AttributedString(clinicAddress);
-        clinicAdd.addAttribute(TextAttribute.FONT, fontClinicAddress);
-        clinicAdd.addAttribute(TextAttribute.FOREGROUND, Color.BLUE);
+        AttributedString doctorDetails = new AttributedString("Dr. " + doctorNm);
+        doctorDetails.addAttribute(TextAttribute.FONT, fontDoctorNameDetails);
+        doctorDetails.addAttribute(TextAttribute.FOREGROUND, Color.BLUE);
         
-        AttributedString clinicCont = new AttributedString(contactNumber);
-        clinicCont.addAttribute(TextAttribute.FONT, fontClinicAddress);
-        clinicCont.addAttribute(TextAttribute.FOREGROUND, Color.BLUE);
+        AttributedString doctorRegLbl = new AttributedString(doctorReg);
+        doctorRegLbl.addAttribute(TextAttribute.FONT, fontDoctorDetails);
+        doctorRegLbl.addAttribute(TextAttribute.FOREGROUND, Color.BLUE);
+        
+        AttributedString doctorDegreeLbl = new AttributedString(doctorDegree);
+        doctorDegreeLbl.addAttribute(TextAttribute.FONT, fontDoctorDetails);
+        doctorDegreeLbl.addAttribute(TextAttribute.FOREGROUND, Color.BLUE);
+        
+        AttributedString doctorContactLbl = new AttributedString(doctorContact + " / " + contactNumber);
+        doctorContactLbl.addAttribute(TextAttribute.FONT, fontDoctorDetails);
+        doctorContactLbl.addAttribute(TextAttribute.FOREGROUND, Color.BLUE);
+        
+        AttributedString doctorEmailLbl = new AttributedString(doctorEmail);
+        doctorEmailLbl.addAttribute(TextAttribute.FONT, fontDoctorDetails);
+        doctorEmailLbl.addAttribute(TextAttribute.FOREGROUND, Color.BLUE);
         
         g.setFont(fontClinicName);
         g.drawString(clinicNm.getIterator(), pageCenter - getLength(clinicName, g) / 2, 20);
-        g.setFont(fontClinicAddress);
-        g.drawString(clinicAdd.getIterator(), pageCenter - getLength(clinicAddress, g) / 2, 35);
-        g.setFont(fontClinicCont);
-        g.drawString(clinicCont.getIterator(), pageCenter - getLength(contactNumber, g) / 2, 55);
-        g.drawLine(10, 60, 550, 60);
+        g.drawLine(10, 95, 550, 95);
+        g.setFont(fontDoctorNameDetails);
+        g.drawString(doctorDetails.getIterator(), 80, 35);
+        g.setFont(fontDoctorDetails);
+        g.drawString(doctorDegreeLbl.getIterator(), 80, 50);
+        g.drawString(doctorRegLbl.getIterator(), 80, 62);
+        g.drawString(doctorContactLbl.getIterator(), 80, 74);
+        g.drawString(doctorEmailLbl.getIterator(), 80, 86);
+        
     }
 
     private int getLength(String str, Graphics g) {
@@ -119,32 +159,13 @@ public class Priscription extends Canvas implements Printable{
     }
 
     private void printPatientInfo(Graphics g, CheckupBean checkup) {
-        String name = checkup.getPatient().getName();
-        String casepapernumber = checkup.getPatient().getCaseId();
+        String name = checkup.getPatient().getName() + "    ( " + checkup.getPatient().getAge() + " / " + checkup.getPatient().getGender() + " )";
         String date = displayDateFormat.format(checkup.getDate());
-        String premedicalHistory = checkup.getPatient().getPreMedicalHistory();
-        String doctorName = checkup.getDentistName();
         int lableX = 20;
         int valueX = 130;
         
         Font fontHeader = new Font("Serif", Font.BOLD, 10);
         Font fontVal = new Font("Serif", Font.PLAIN, 10);
-
-        AttributedString doctorNmLbl = new AttributedString("Doctor Name  :    ");
-        doctorNmLbl.addAttribute(TextAttribute.FONT, fontHeader);
-        doctorNmLbl.addAttribute(TextAttribute.FOREGROUND, Color.BLACK);
-        
-        AttributedString doctorNm = new AttributedString(doctorName);
-        doctorNm.addAttribute(TextAttribute.FONT, fontVal);
-        doctorNm.addAttribute(TextAttribute.FOREGROUND, Color.BLACK);
-        
-        AttributedString dateLbl = new AttributedString("Date :    ");
-        dateLbl.addAttribute(TextAttribute.FONT, fontHeader);
-        dateLbl.addAttribute(TextAttribute.FOREGROUND, Color.BLACK);
-        
-        AttributedString date1 = new AttributedString(date);
-        date1.addAttribute(TextAttribute.FONT, fontVal);
-        date1.addAttribute(TextAttribute.FOREGROUND, Color.BLACK);
         
         AttributedString patientNmLbl = new AttributedString("Patient Name :    ");
         patientNmLbl.addAttribute(TextAttribute.FONT, fontHeader);
@@ -153,33 +174,21 @@ public class Priscription extends Canvas implements Printable{
         AttributedString patientNm = new AttributedString(name);
         patientNm.addAttribute(TextAttribute.FONT, fontVal);
         patientNm.addAttribute(TextAttribute.FOREGROUND, Color.BLACK);
+
+        AttributedString dateLbl = new AttributedString("Date :    ");
+        dateLbl.addAttribute(TextAttribute.FONT, fontHeader);
+        dateLbl.addAttribute(TextAttribute.FOREGROUND, Color.BLACK);
         
-        AttributedString casePaperLbl = new AttributedString("Case Paper No  :    ");
-        casePaperLbl.addAttribute(TextAttribute.FONT, fontHeader);
-        casePaperLbl.addAttribute(TextAttribute.FOREGROUND, Color.BLACK);
+        AttributedString date1 = new AttributedString(date);
+        date1.addAttribute(TextAttribute.FONT, fontVal);
+        date1.addAttribute(TextAttribute.FOREGROUND, Color.BLACK);
         
-        AttributedString casepep = new AttributedString(casepapernumber);
-        casepep.addAttribute(TextAttribute.FONT, fontVal);
-        casepep.addAttribute(TextAttribute.FOREGROUND, Color.BLACK);
         
-        AttributedString preMedicalHistLbl = new AttributedString("Pre Medical History :    ");
-        preMedicalHistLbl.addAttribute(TextAttribute.FONT, fontHeader);
-        preMedicalHistLbl.addAttribute(TextAttribute.FOREGROUND, Color.BLACK);
+        g.drawString(patientNmLbl.getIterator(), 80, 110);
+        g.drawString(patientNm.getIterator(), 150, 110);
+        g.drawString(dateLbl.getIterator(), 410, 110);
+        g.drawString(date1.getIterator(), 450, 110);
         
-        AttributedString preMedicalHist = new AttributedString(premedicalHistory);
-        preMedicalHist.addAttribute(TextAttribute.FONT, fontVal);
-        preMedicalHist.addAttribute(TextAttribute.FOREGROUND, Color.BLACK);
-        
-        g.drawString(doctorNmLbl.getIterator(), lableX, 75);
-        g.drawString(doctorNm.getIterator(), valueX, 75);
-        g.drawString(dateLbl.getIterator(), 320, 75);
-        g.drawString(date1.getIterator(), 420, 75);
-        g.drawString(patientNmLbl.getIterator(), lableX, 95);
-        g.drawString(patientNm.getIterator(), valueX, 95);
-        g.drawString(casePaperLbl.getIterator(), 320, 95);
-        g.drawString(casepep.getIterator(), 420, 95);
-        g.drawString(preMedicalHistLbl.getIterator(), lableX, 115);
-        g.drawString(preMedicalHist.getIterator(), valueX, 115);
     }
 
     public void printPriscription() {
@@ -193,7 +202,7 @@ public class Priscription extends Canvas implements Printable{
     }
 
     private void printMedicine(Graphics2D g, CheckupBean checkup) {
-        int priscriptionRowStart = 150;
+        int priscriptionRowStart = 140;
 
         Font fontHeader = new Font("Serif", Font.BOLD, 12);
         Font fontVal = new Font("Serif", Font.PLAIN, 10);
@@ -213,7 +222,7 @@ public class Priscription extends Canvas implements Printable{
                 String medicineTypeSelected = priscription.getMedicineType();
                 String medicineName = priscription.getMedicineName();
                 String medicineStrength = priscription.getMedicineStrength();
-                String medicineFrequency = priscription.getFrequency();
+                String medicineFrequency = getFrequency(priscription.getFrequency());
                 String medicineDuration = priscription.getDuration();
                 g.drawString(medicineTypeSelected == null ? "" : "(" + medicineTypeSelected + ")", 90, rowXpoint);
                 g.drawString(medicineName == null ? "N/A" : medicineName + " " + (medicineStrength == null || medicineStrength.isEmpty() ? "" : " - " + medicineStrength), 140, rowXpoint);
@@ -223,6 +232,21 @@ public class Priscription extends Canvas implements Printable{
         }
         
         g.drawLine(80, rowXpoint + 20, 500, rowXpoint + 20);
+    }
+    
+    public static void main(String args []){
+        Priscription canvas = new Priscription(22432);
+        canvas.printPriscription();
+    }
+
+    private String getFrequency(String frequency) {
+        String frq = frequency;
+        if(frequency.endsWith("BM")){
+            frq = frequency.replaceFirst("BM", "Before Meal");
+        } else if (frequency.endsWith("AM")){
+            frq = frequency.replaceFirst("AM", "After Meal");
+        }
+        return frq;
     }
   
 }
